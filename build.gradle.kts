17b6566acceb6dec60d37db845e9e534f4711f84
aragabz <ahmedragab.se@gmail.com>
2025-06-16 02:09:00 +0300

    remove spring boot docker compose support

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+//	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/aragabz/spkt/SpktApplication.kt
+++ src/main/kotlin/com/aragabz/spkt/SpktApplication.kt
@@ -2,6 +2,8 @@ package com.aragabz.spkt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class SpktApplication
