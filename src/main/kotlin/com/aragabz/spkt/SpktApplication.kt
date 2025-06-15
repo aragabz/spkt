2c483ea030b3d02870bc883380db0004ec544f15
aragabz <ahmedragab.se@gmail.com>
2025-06-16 01:55:22 +0300

    create project
    # Conflicts:
    #	.gitignore

---
+++ src/main/kotlin/com/aragabz/spkt/SpktApplication.kt
@@ -0,0 +1,11 @@
+package com.aragabz.spkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpktApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpktApplication>(*args)
+}
---
+++ src/test/kotlin/com/aragabz/spkt/SpktApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aragabz.spkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpktApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
